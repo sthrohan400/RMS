ce7d93bce36d82c62bdbc176d4d70e662eaebb8a
RohanStha <stharohan400@gmail.com>
2017-02-01 15:10:56 +0545

    Restaurant Management System Initiation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+    {
+      path: '',
+      redirectTo: '/app',
+      pathMatch: 'full'
+    },
+    {
+      path: 'app',
+      component: AppComponent
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
